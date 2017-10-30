7ab034997ae042111e084d3268a0f9ca85358d1b
anyanelson <anelson@newroads.org>
2017-10-30 09:41:00 -0700

    added o

--- tictactoescript.js
+++ tictactoescript.js
@@ -3,4 +3,14 @@ var character = 'X';
 //This function handles the onlcik event
 function turn (location) {
 	document.getElementById(location).innerHTML = character;
+
+	//switch between X and O
+	if (character=='X') {
+		character='O';
+	}
+	else{
+		character='X';
+	}
+
+	document.getElementById(location).innerHTML=character;
 }
