ea5a43cab28d96837eaf48a3b9e16992e40f0e42
jbeatts <jbeatts@newroads.org>
2017-11-27 09:47:16 -0800

    finished tic tac toe & created clickable

---
+++ clickable.js
@@ -0,0 +1,28 @@
+
+//draws random circle in ClickPanel
+function randomCircle () {
+
+	//randomly get top positions
+	document.getElementById("circle").style.visibility = "appear";
+	document.getElementById("clickPanel").style.top = "200px";
+
+
+	//randomly get left positions
+	document.getElementById("clickPanel").style.left = "200px";
+	
+
+	//update position of circle
+
+}
+
+//handles onclick event
+//when random circle is click in clickPanel, it disappears
+//then generates a new random circle
+function clickCircle () {
+
+	//circle is clicked, hide circle
+	document.getElementById("circle").style.visibility = "hidden";
+
+	//generate random circle
+ 
+}
--- tictactoescript.js
+++ tictactoescript.js
@@ -1,10 +1,12 @@
 var character = 'X';
+var gameOver = false;
 
-//This function handles the onlcik event
-function turn (location) {
+//This function handles the onclik event
+function turn(location) {
 
 	//update box with character, if box is empty
-	if(document.getElementById(location).innerHTML == "") {
+	if(document.getElementById(location).innerHTML == "" &&
+		gameOver == false) {
 
 	//update results div with feedback
 	document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
@@ -17,11 +19,64 @@ function turn (location) {
 		character='X';
 	}
 
-	//write box in character
+	//write character in box
 	document.getElementById(location).innerHTML = character;
-}
-
+	//check for winner after move
+	checkWinner();
 
 	}
+
+}
+
+//checks for winner
+function checkWinner() {
+
+	//grab HTML of each tic tac toe div (which holds x or o)
+	var r1c1 = document.getElementById("r1c1").innerHTML;
+	var r1c2 = document.getElementById("r1c2").innerHTML;
+	var r1c3 = document.getElementById("r1c3").innerHTML;
+	var r2c1 = document.getElementById("r2c1").innerHTML;
+	var r2c2 = document.getElementById("r2c2").innerHTML;
+	var r2c3 = document.getElementById("r2c3").innerHTML;
+	var r3c1 = document.getElementById("r3c1").innerHTML;
+	var r3c2 = document.getElementById("r3c2").innerHTML;
+	var r3c3 = document.getElementById("r3c3").innerHTML;
+
+	if(((r1c1 == r1c2) && (r1c1 == r1c3) && r1c1 != '') || //first row
+	   ((r2c1 == r2c2) && (r2c1 == r2c3) && r2c1 != '') || //second row
+	   ((r3c1 == r3c2) && (r3c1 == r3c3) && r3c1 != '') || //third row
+	   ((r1c1 == r2c1) && (r1c1 == r3c1) && r1c1 != '') || // first column
+	   ((r1c2 == r2c2) && (r3c2 == r2c2) && r2c2 != '') || //second column
+	   ((r1c3 == r2c3) && (r3c3 == r2c3) && r2c3 != '') || //third column
+	   ((r1c1 == r2c2) && (r1c1 == r3c3) && r1c1 != '') || //left to right diagonal
+	   ((r1c3 == r2c2) && (r3c1 == r2c2) && r2c2 != '')) { //right to left diagonal
+
+	//game is over now, there's a winner
+	gameOver= true;
+
+	//update results div
+	document.getElementById("results").innerHTML = character + " wins!";
+}
+
+	}
+
+//clear tic tac toe board
+function clearAll(){
+
+	//clear all 9 boxes
+	document.getElementById("r1c1").innerHTML="";
+	document.getElementById("r1c2").innerHTML="";
+	document.getElementById("r1c3").innerHTML="";
+	document.getElementById("r2c1").innerHTML="";
+	document.getElementById("r2c2").innerHTML="";
+	document.getElementById("r2c3").innerHTML="";
+	document.getElementById("r3c1").innerHTML="";
+	document.getElementById("r3c2").innerHTML="";
+	document.getElementById("r3c3").innerHTML="";
+
+	//reseet gameOver so players can play again
+	gameOver = false;
+}
+
 
 	
