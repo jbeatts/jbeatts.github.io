b05d561ca548f4eb8adbb5ec3366cd81ae0b5d99
jbeatts <jbeatts@newroads.org>
2017-12-04 09:02:12 -0800

    finished clickale

--- clickable.js
+++ clickable.js
@@ -2,17 +2,16 @@
 //draws random circle in ClickPanel
 function randomCircle () {
 
+	//update position of circle
 	//randomly get top positions
-	document.getElementById("circle").style.visibility = "appear";
-	document.getElementById("clickPanel").style.top = "200px";
+	document.getElementById("circle").style.top = randomNum();
 
+	//document.getElementById("circle").style.top = "200px";
 
 	//randomly get left positions
-	document.getElementById("clickPanel").style.left = "200px";
+	document.getElementById("circle").style.left = randomNum();
+	//document.getElementById("circle").style.left = "200px";	
 	
-
-	//update position of circle
-
 }
 
 //handles onclick event
@@ -21,8 +20,16 @@ function randomCircle () {
 function clickCircle () {
 
 	//circle is clicked, hide circle
-	document.getElementById("circle").style.visibility = "hidden";
+	//document.getElementById("circle").style.visibility = "hidden";
 
 	//generate random circle
- 
+	//document.getElementById("circle").style.visibility = "visible";
+	
+ 	//move circle
+ 	randomCircle ();
+}
+
+function randomNum() {
+	return Math.floor (Math.random() * 200);
+
 }
