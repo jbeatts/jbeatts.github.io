587f825dfb6eac5c9953f079f1cf4fc2d55b745d
jbeatts <jbeatts@newroads.org>
2017-12-11 09:06:50 -0800

    finished gallery

--- gallery.js
+++ gallery.js
@@ -2,5 +2,20 @@ var list = ["a1.jpg",
 			"a2.jpg",
 			"a3.jpg",
 			"a4.jpg",
-			"a5.jpg",];
+			"a5.jpg"];
 
+var imagevalue =0;
+
+function prev () {
+	if (imagevalue>0) {
+		imagevalue=imagevalue-1;
+		document.getElementById("a1").src =list[imagevalue]
+	}
+}
+
+function next () {
+	if (imagevalue<2){
+		imagevalue=imagevalue+1;
+		document.getElementById("a1").src = list[imagevalue];
+	}
+}
