fa5ff97744148fb75f6f8b12371e29c38f1c1852
Jaylene <jaylenebeatts@Jaylenes-MacBook-Pro.local>
2018-02-25 18:30:52 -0800

    added a profile & made index not ugly

---
+++ dogprofile.js
@@ -0,0 +1,13 @@
+
+
+//Variables here:
+//(alongside with commented suggestions)
+$foreground-color:#b85b3f;//black;
+$background-color:#e8e3c7;//white
+$shadow-color:#ba9186;//$foreground-color;
+$distance:8px;
+$cut-distance:3px;//$distance/4;
+$strips-size:6px; //10px
+$strips-ratio:50%;//70%
+$strips-angle:45deg;//90deg;
+
