da1f3dfa88603c4d16289d992123896f6de0a069
Jaylene <jaylenebeatts@Jaylenes-MacBook-Pro.local>
2018-03-05 09:27:05 -0800

    added search categories

---
+++ searchbar.js
@@ -0,0 +1,14 @@
+
+//Checkbox: <input type="checkbox" id="myCheck">
+
+//<button onclick="check()">Check Checkbox</button>
+//<button onclick="uncheck()">Uncheck Checkbox</button>
+
+
+function check() {
+    document.getElementById("myCheck").checked = true;
+}
+
+function uncheck() {
+    document.getElementById("myCheck").checked = false;
+}
